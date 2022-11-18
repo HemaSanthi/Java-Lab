0e00a74428048b2c81e5c2dae351424ff78494ef
Hema Santhi <116088804+HemaSanthi@users.noreply.github.com>
2022-11-18 10:25:24 +0530

    Create StringFunctions.java

---
+++ StringFunctions.java
@@ -0,0 +1,43 @@
+package string;
+
+public class StringFunctions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     //create strings
+        String s1="Java";
+        String s2=" Programming";
+        char arr[]={'i','s','g','o','o','d'};
+        String s3= new String(arr);
+
+        //display all three strings
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        //length
+        System.out.println("Length of s1 is:"+s1.length());
+        System.out.println("Length of s2 is:"+s2.length());
+        System.out.println("Length of s3 is:"+s3.length());
+
+        //concatenate
+        System.out.println("s1 + s2 is:"+ s1.concat(s2));
+        System.out.println("s1 + s3:"+s1.concat(s3));
+
+        //sub string
+        System.out.println("Sub String in s2:"+s2.substring(0,8));
+
+        //to upper case
+        System.out.println("To uppercase of s1:"+s1.toUpperCase());
+
+        //to lower case;
+        System.out.println("Lowercase of s1 is:"+s1.toLowerCase());
+        // index of character
+        System.out.println("Index Of v is "+s1.indexOf('v'));
+
+    }
+
+
+	
+
+}
